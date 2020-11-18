410b8d4606c2e482a5decb0433b13397a619d0f7
BetoCruz <humbertocruz.sp@gmail.com>
2020-11-18 15:28:01 -0300

    Injeçao do serviço alert.service de e sua declaração em providers para funcionamento global e uso por quaisquer componentes

--- LivroAngular/src/app/app.module.ts
+++ LivroAngular/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
 import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
+import { AlertaService } from './alerta.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,7 @@ import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [AlertaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
